df92b1706f014e5449fdd0c5ef19c7689abce492
Rex <rexlucien@hotmail.com>
2016-08-20 21:47:36 +0800

    Add project files.

---
+++ Web948/Scripts/dist/app.js
@@ -0,0 +1,51 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var Good = (function () {
+        function Good() {
+        }
+        return Good;
+    }());
+    exports.Good = Good;
+    var Company = (function () {
+        function Company() {
+        }
+        return Company;
+    }());
+    exports.Company = Company;
+    var App = (function () {
+        function App() {
+            this.vm = [];
+        }
+        App.prototype.init = function () {
+            var _this = this;
+            console.log("init");
+            this.height = $(".mobile").height();
+            console.log(this.height);
+            var homeBlock = $(".mobile .home");
+            var menuBlock = $(".mobile .menu");
+            var btns = $(".mobile a");
+            console.log(btns);
+            btns.click(function () {
+                homeBlock.css({
+                    "display": "none"
+                });
+                var height = _this.height - (54 * 2);
+                $(".menu .menu-area").css({
+                    "height": "" + height
+                });
+                menuBlock.show();
+            });
+            var homeBtn = $(".mobile a[value=Home]");
+            var menuBtn = $(".mobile a[value=Menu]");
+            var detailBtn = $(".mobile a[value=Detail]");
+            var detailBlock = $(".mobile .detail");
+            console.log(homeBtn);
+            console.log(menuBtn);
+            console.log(detailBtn);
+        };
+        return App;
+    }());
+    exports.App = App;
+    var app = new App();
+    app.init();
+});
---
+++ Web948/Scripts/dist/app.ts
@@ -0,0 +1,57 @@
+﻿export class Good {
+    name: string;
+    price: string;
+    url: string;
+}
+
+export class Company {
+    name: string;
+    goods: Good[];
+}
+
+export class App {
+    private vm: Company[] = [];
+
+    private height: number;
+
+    init() {
+        console.log("init");
+
+        this.height = $(".mobile").height();
+        console.log(this.height);
+
+        const homeBlock = $(".mobile .home");
+        const menuBlock = $(".mobile .menu");
+
+        const btns = $(".mobile a");
+        console.log(btns);
+        btns.click(() => {
+            homeBlock.css({
+                "display": "none"
+            });
+
+            const height = this.height - (54 * 2);
+            $(".menu .menu-area").css({
+                "height": `${height}`
+            });
+            menuBlock.show();
+        });
+
+        const homeBtn = $(".mobile a[value=Home]");
+
+        const menuBtn = $(".mobile a[value=Menu]");
+
+        const detailBtn = $(".mobile a[value=Detail]");
+
+        const detailBlock = $(".mobile .detail");
+
+        console.log(homeBtn);
+
+        console.log(menuBtn);
+
+        console.log(detailBtn);
+    }
+}
+
+var app = new App();
+app.init();
